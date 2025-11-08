93f58c719aba1c6b015f22c68f52b56ab8fadfe1
lernwithshubham <munshaeservices@gmail.com>
2025-11-08 08:25:12 +0530

    Update AWS region and add AMI ID variable
    Changed default AWS region from 'us-east-1' to 'us-east-2' and added new variable 'ami_id' for EC2.

--- variables.tf
+++ variables.tf
@@ -1,10 +1,16 @@
 variable "aws_region" {
   description = "AWS region to deploy infrastructure"
   type = string
-  default = "us-east-1"
+  default = "us-east-2"
 }
 variable "instance_type" {
   description = "Instance type for EC2"
   type = string
   default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon machine Image for EC2"
+  type = string
+  default = "ami-0c5ddb3560e768732"
 }
